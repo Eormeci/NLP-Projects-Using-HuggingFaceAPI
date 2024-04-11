14b8b424ecd280e7c224a99e2322d1cbaf2cc17b
Eormeci <enhar.ormeci@stu.enka.k12.tr>
2024-04-11 23:37:43 +0300

    NLP Projects
    In these projects, I have worked on tasks such as question answering, text generation, summarization, and English to SQL translation using the HuggingFaceAPI. Additionally, I have conducted vector similarity search using ChromaDB, a vector database.

---
+++ question_answering.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from langchain import HuggingFaceHub,LLMChain
+from langchain.prompts import PromptTemplate
+
+load_dotenv()
+
+hub_llm = HuggingFaceHub(repo_id="google/gemma-1.1-7b-it")
+
+prompt = PromptTemplate(
+    input_variables=["text-generation"],
+    template="Text generation :{text-generation}"
+    )
+
+hub_chain = LLMChain(prompt=prompt,llm =hub_llm,verbose=True)
+print(hub_chain.run("How does the brain work ? "))
+
+ 
